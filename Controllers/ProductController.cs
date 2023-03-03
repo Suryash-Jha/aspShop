a762446cb30551165a3e39e5c7e4496b3c24fb3a
Suryash <joblesscoder2468@gmail.com>
2023-03-03 22:24:10 +0530

    Redirect to result added

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using aspShop.Models;
+
+namespace aspShop.Controllers;
+
+public class ProductController : Controller
+{
+    private readonly ILogger<ProductController> _logger;
+
+    public ProductController(ILogger<ProductController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult ShowProd(string id){
+        Console.WriteLine(id+id);
+        return View();
+    }
+
+    public IActionResult AddProd([FromForm] Product product)
+    {
+        string name = product.Name;
+        string price = product.Price;
+        string quantity = product.Quantity;
+        string description = product.Description;
+        string category = product.Category;
+        if(name=="f")
+            return RedirectToAction("ShowProd", "Product", new { id = name });
+        // ShowProd(name);
+        // Console.WriteLine(name);
+        // Console.WriteLine(price);
+
+        
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/ProductDetailModel.cs
@@ -0,0 +1,13 @@
+namespace aspShop.Models
+{
+
+    public class Product
+    {
+        public string Name { get; set; }
+        public string Price { get; set; }
+        public string Quantity { get; set; }
+        public string Description { get; set; }
+        public string Category {get; set; }
+
+    }
+}
