3fe983afa9917544f0940b58af13b2fc5e520b16
Suryash <joblesscoder2468@gmail.com>
2023-03-05 21:08:58 +0530

    Db connection established

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,6 +1,7 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using aspShop.Models;
+using MySql.Data.MySqlClient;
 
 namespace aspShop.Controllers;
 
@@ -15,6 +16,24 @@ public class ProductController : Controller
 
     public IActionResult Index()
     {
+        MySqlConnection conn = new MySqlConnection("server=localhost;user=root;database=shopApp;port=3306;password=Lion@123");
+        // MySqlConnection conn = new MySqlConnection("server=localhost;user=root;database=studentData;port=3306;password=Lion@123");
+
+        // MySqlConnection con = new MySqlConnection("server=localhost;user=root;database=shopApp;port=3306;password=Lion@123");
+        conn.Open();
+        MySqlCommand cmd = new MySqlCommand("SELECT * FROM productDetails;", conn);
+        MySqlDataReader dr = cmd.ExecuteReader();
+        while (dr.Read())
+        {
+            Console.WriteLine(dr.GetString(0));
+            Console.WriteLine(dr.GetString(1));
+            Console.WriteLine(dr.GetString(2));
+            // Console.WriteLine(dr.GetString(3));
+            // Console.WriteLine(dr.GetString(4));
+        }
+        dr.Close();
+        cmd.Dispose();
+        conn.Close();
         return View();
     }
 
@@ -31,7 +50,7 @@ public class ProductController : Controller
         string quantity = product.Quantity;
         string description = product.Description;
         string category = product.Category;
-        ViewBag.nameVal= name;
+        ViewBag.nameVal = name;
         if (name == "f")
             return RedirectToAction("ShowProd", "Product", new { id = name });
         // return ShowProd(name);
